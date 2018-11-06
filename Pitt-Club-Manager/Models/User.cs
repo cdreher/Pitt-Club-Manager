30c8420a9e66b5b37ba652166ea194dc8ff63b4a
Joshua Maciak <jtm97@pitt.edu>
2018-11-06 16:46:53 -0500

    removed userpermission field. might not be necessary but we can worry about that later

--- Pitt-Club-Manager/Models/User.cs
+++ Pitt-Club-Manager/Models/User.cs
@@ -4,14 +4,12 @@ namespace PittClubManager.Models
     public class User
     {
         private int uid;
-        private UserPermission permission;
         private string firstName;
         private string lastName;
 
-        public User(int uid, UserPermission permission, string firstName, string lastName)
+        public User(int uid, string firstName, string lastName)
         {
             this.uid = uid;
-            this.permission = permission;
             this.firstName = firstName;
             this.lastName = lastName;
         }
