310a01ccc5aee20415e73bf71616ad524f78ea96
joshuamaciak <jtm97@pitt.edu>
2018-11-28 21:29:57 -0500

    added superuser ability to approve pending clubs

---
+++ Pitt-Club-Manager/Controllers/SuperuserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PittClubManager.Util;
+namespace PittClubManager.Controllers
+{
+    public class SuperuserController : Controller
+    {
+        // GET: Superuser
+        public ActionResult Index()
+        {
+            var pendingClubs = FirebaseHelper.GetPendingClubs();
+            return View(pendingClubs);
+        }
+
+        public ActionResult ApproveClub(string id)
+        {
+            FirebaseHelper.ApproveClub(id);
+            return View();
+        }
+    }
+}
--- Pitt-Club-Manager/Util/FirebaseHelper.cs
+++ Pitt-Club-Manager/Util/FirebaseHelper.cs
@@ -16,7 +16,7 @@ namespace PittClubManager.Util
         public const string DB_NAME = "pitt-club-manager";
         public const string COLLECTION_CLUBS = "clubs";
         public const string COLLECTION_USERS = "users";
-
+        public const string COLLECTION_PENDING_CLUBS = "pendingClubs";
         public const string EMAIL = "jmaciak14@gmail.com";
         public const string PASSWORD = "password";
         public const string WEB_KEY = "AIzaSyCN8Av2-nfNtsRdlWaZiaejPdwQ4QqA38c";
@@ -120,6 +120,60 @@ namespace PittClubManager.Util
             return (PittClubManager.Models.User[]) users.ToArray(typeof(PittClubManager.Models.User));
         }
 
+        public static void CreateClub(String clubName, String description, String managerId)
+        {
+            FirebaseClient firebase = new FirebaseClient(FIREBASE_URL);
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WEB_KEY));
+            FirebaseAuthLink res = authProvider.SignInWithEmailAndPasswordAsync(EMAIL, PASSWORD).Result;
+            FirestoreDb db = FirestoreDb.Create(DB_NAME);
+            // todo: check to make sure this club doesn't already exist in created clubs
+            var result = db.Collection(COLLECTION_PENDING_CLUBS).AddAsync(new Dictionary<String, String>()
+            {
+                {"name", clubName},
+                {"description", description},
+                {"managerId", managerId}
+            }).Result;
+        }
+
+        public static Club[] GetPendingClubs()
+        {
+            FirebaseClient firebase = new FirebaseClient(FIREBASE_URL);
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WEB_KEY));
+            FirebaseAuthLink res = authProvider.SignInWithEmailAndPasswordAsync(EMAIL, PASSWORD).Result;
+            FirestoreDb db = FirestoreDb.Create(DB_NAME);
+            var snap = db.Collection(COLLECTION_PENDING_CLUBS).GetSnapshotAsync().Result;
+            ArrayList pendingClubs = new ArrayList();
+            foreach (var doc in snap.Documents)
+            {
+                        pendingClubs.Add(PendingClubSnapshotToClub(doc));                
+            }
+            return (PittClubManager.Models.Club[])pendingClubs.ToArray(typeof(PittClubManager.Models.Club));
+        }
+
+        public static void ApproveClub(String clubId)
+        {
+            FirebaseClient firebase = new FirebaseClient(FIREBASE_URL);
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WEB_KEY));
+            FirebaseAuthLink res = authProvider.SignInWithEmailAndPasswordAsync(EMAIL, PASSWORD).Result;
+            FirestoreDb db = FirestoreDb.Create(DB_NAME);
+            DocumentReference clubReference = db.Collection(COLLECTION_PENDING_CLUBS).Document(clubId);
+            var snap = clubReference.GetSnapshotAsync().Result;
+            if (snap.Exists)
+            {
+                var result = db.Collection(COLLECTION_CLUBS).AddAsync(new Dictionary<string, object>
+                    {
+                        {"name", snap.GetValue<string>("name")},
+                        {"description", snap.GetValue<string>("description")},
+                        {"managerId", snap.GetValue<string>("managerId")},
+                        {"filter", "THIS NEEDS TO BE IMPLEMENTED"},
+                        {"eventIds", new string[0]},
+                        {"memberIds",new string[0]}
+                    }).Result;
+                // make sure to remove club from pending after approval
+                var deleted = clubReference.DeleteAsync().Result;
+            }
+        }
+
         public static PittClubManager.Models.User GetUser(string id)
         {
             FirebaseClient firebase = new FirebaseClient(FIREBASE_URL);
@@ -157,6 +211,18 @@ namespace PittClubManager.Util
             string[] memberIds = snap.GetValue<string[]>("memberIds");
             PittClubManager.Models.User[] members = GetUsersFromIds(memberIds);
             c.SetMembers(members);
+            return c;
+        }
+
+        private static Club PendingClubSnapshotToClub(DocumentSnapshot snap)
+        {
+            Club c = new Club();
+            c.SetName(snap.GetValue<string>("name"));
+            c.SetId(snap.Id);
+            c.SetDescription(snap.GetValue<string>("description"));
+            String managerId = snap.GetValue<string>("managerId");
+            PittClubManager.Models.User manager = GetUser(managerId);
+            c.SetManager(manager);
             return c;
         }
     }
